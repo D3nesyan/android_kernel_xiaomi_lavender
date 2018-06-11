ac55157a6c326c59492279f3a9d251c33d1c39ee
Wei Wang <wvw@google.com>
2018-06-11 16:14:51 -0700

    kernel: initialize and free cpufreq stats properly
    Initialize task's cpufreq to NULL including for idle
    Make sure free task's cpufreq when free task struct
    
    Bug: 110044919
    Change-Id: Ie4629d0ebe3ef4b72dffea3ee613b15f40a57142
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -78,6 +78,7 @@
 #include <linux/compiler.h>
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
+#include <linux/cpufreq.h>
 #include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
@@ -229,6 +230,9 @@ static void account_kernel_stack(unsigned long *stack, int account)
 
 void release_task_stack(struct task_struct *tsk)
 {
+#ifdef CONFIG_CPU_FREQ_STAT
+	cpufreq_task_stats_free(tsk);
+#endif
 	account_kernel_stack(tsk->stack, -1);
 	arch_release_thread_stack(tsk->stack);
 	free_thread_stack(tsk->stack);
