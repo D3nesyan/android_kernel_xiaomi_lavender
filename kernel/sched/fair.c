a0034930f8ad84e54795247722310d5bdfbfef81
Todd Kjos <tkjos@google.com>
2018-06-20 15:38:28 -0700

    ANDROID: sched/fair: if sync flag ignored, try to place in same cluster
    If the sync flag is ignored because the current cpu is not in
    the affinity mask for the target of a sync wakeup (usually
    binder call), prefer to place in the same cluster if
    possible. The main case is a "top-app" task waking a
    "foreground" task when the top-app task is running on
    a CPU that is not in the foreground cpuset. This patch
    causes the "top-app" search order be used (big cpus first)
    even if schedtune.boost is 0 when the sync flag failed.
    
    Bug: 110464019
    Change-Id: Ib026231cee5c8b9d616d4ad4b92dd8502e79d0c0
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Keep changes from 9f7516a28d7ca45d2d69352d70f4df4365041dd4.
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7243,7 +7243,8 @@ static int wake_cap(struct task_struct *p, int cpu, int prev_cpu)
 	return min_cap * 1024 < task_util_est(p) * capacity_margin;
 }
 
-static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu)
+static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu,
+				   int sync_boost)
 {
 	bool boosted, prefer_idle;
 	struct sched_domain *sd;
@@ -7271,7 +7272,8 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu)
 	sync_entity_load_avg(&p->se);
 
 	/* Find a cpu with sufficient capacity */
-	next_cpu = find_best_target(p, &backup_cpu, boosted, prefer_idle);
+	next_cpu = find_best_target(p, &backup_cpu, boosted || sync_boost,
+				    prefer_idle);
 	if (next_cpu == -1) {
 		target_cpu = prev_cpu;
 		goto out;
@@ -7385,7 +7387,17 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	rcu_read_lock();
 	sd = rcu_dereference(cpu_rq(prev_cpu)->sd);
 	if (energy_aware() && sd && !sd_overutilized(sd)) {
-		new_cpu = select_energy_cpu_brute(p, prev_cpu);
+		/*
+		 * If the sync flag is set but ignored, prefer to
+		 * select cpu in the same cluster as current. So
+		 * if current is a big cpu and sync is set, indicate
+		 * that the selection algorithm for a boosted task
+		 * should be used.
+		 */
+		bool sync_boost = sync && cpu >= start_cpu(true);
+
+		new_cpu = select_energy_cpu_brute(p, prev_cpu, sync_boost);
+
 		goto unlock;
 	}
 
@@ -10740,7 +10752,7 @@ void check_for_migration(struct rq *rq, struct task_struct *p)
 		    rq->curr->nr_cpus_allowed == 1)
 			return;
 
-		new_cpu = select_energy_cpu_brute(p, cpu);
+		new_cpu = select_energy_cpu_brute(p, cpu, 0);
 		if (capacity_orig_of(new_cpu) > capacity_orig_of(cpu)) {
 			active_balance = kick_active_balance(rq, p, new_cpu);
 			if (active_balance)
